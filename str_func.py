da749b45e2be6ab980923c2b22126e8ff688eec5
Your Name <you@example.com>
2023-10-22 17:48:28 +0300

    add str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
 def make_string_uppercase(input_string):
     """Функция, которая принимает строку и возвращает ее в верхнем регистре"""
     return input_string.upper()
+
+def capitalize_words(input_string):
+    """
+    Функция, которая принимает строку и возвращает ее с заглавными первыми буквами каждого слова.
+    """
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
